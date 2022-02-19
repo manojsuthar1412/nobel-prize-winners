afb6dbf1a22081c9f3d879410811176979826adb
manojsuthar1412 <msuthar1412@gmail.com>
2022-02-19 07:54:53 +0530

    with axios

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,17 +1,23 @@
-export const priceList = () => {
-  return fetch(`http://api.nobelprize.org/v1/prize.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      data = data.prizes.filter(
-        (item) => item.year >= 1900 && item.year <= 2018
-      );
-      let dataList = [];
+import axios from "axios";
 
-      // console.log("RAW: ", data);
+export const priceList = async () => {
+  // res.prizes.filter((item) => item.year >= 1900 && item.year <= 2018);
+  let res = await axios.get(`http://api.nobelprize.org/v1/prize.json`);
+  return res.data.prizes.filter(
+    (item) => item.year >= 1900 && item.year <= 2018
+  );
+  // console.log(res.data.prizes);
+  // res
+  //   .then((data) => {
+  //     data = data.prizes.filter(
+  //       (item) => item.year >= 1900 && item.year <= 2018
+  //     );
 
-      return data;
-    })
-    .catch((err) => console.log(err));
+  //     // console.log("RAW: ", data);
+
+  //     return data;
+  //   })
+  //   .catch((err) => console.log(err));
 };
 
 export default priceList;
